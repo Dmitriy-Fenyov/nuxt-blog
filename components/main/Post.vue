ff6017b5d1e3fa0f8035586e661021e18fc1fd2f
Dmitriy <rsp.fdrf@yandex.ru>
2022-08-21 22:56:28 +0500

    Создан компонент с постами

---
+++ components/main/Post.vue
@@ -0,0 +1,59 @@
+<template>
+  <el-card shadow="hover" :body-style="{padding: 0}" class="post">
+    <header slot="header" class="post-header">
+      <h3>Post title</h3>
+      <small>
+        <i class="el-icon-time"></i>
+        {{ new Date().toLocaleString() }}</small>
+    </header>
+    <div class="post-body">
+      <img
+      src="https://media.1777.ru/images/images_processing/173/1735789010088582.jpeg"
+      alt="post image"
+      class="post-img">
+    </div>
+    <footer class="post-footer">
+      <el-button round @click="openPost">Открыть </el-button>
+
+      <span>
+        <i class="el-icon-message"></i>
+        12
+      </span>
+    </footer>
+  </el-card>
+</template>
+
+<script>
+export default {
+  methods: {
+    openPost() {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .post {
+    margin-bottom: 1.5rem;
+  }
+
+  .post-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+.post-img {
+  width: 100%;
+  height: auto;
+}
+
+.post-footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+}
+
+</style>
