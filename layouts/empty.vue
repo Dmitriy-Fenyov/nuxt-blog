aaeaa73f03d00c11a84a6918161c5fd901041da4
Dmitriy <rsp.fdrf@yandex.ru>
2022-08-27 18:33:20 +0500

    добавлен обработчик ошибок

--- layouts/empty.vue
+++ layouts/empty.vue
@@ -4,6 +4,26 @@
   </div>
 </template>
 
+
+<script>
+
+
+export default {
+ computed: {
+    error() {
+      this.$store.getters.error
+    }
+ },
+ watch: {
+  error(value) {
+    this.$message.error(value)
+  }
+ }
+}
+
+</script>
+
+
 <style lang="scss" scoped>
   .empty-layout {
     display: flex;
