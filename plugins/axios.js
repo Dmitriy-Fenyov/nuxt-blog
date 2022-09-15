7db98712e82ad3b5d37f3e6c6adddebbe07be711
Dmitriy <rsp.fdrf@yandex.ru>
2022-09-15 19:57:03 +0500

    добавил токен к запросам

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,4 +1,15 @@
 export default function ({ $axios, redirect, store }) {
+
+  $axios.interceptors.request.use(request => {
+
+    if (store.getters['auth/isAuthenticated'] && !request.headers.common['Authorization']) {
+      const token = store.getters['auth/token']
+      request.headers.common['Authorization'] = `Bearer ${token}`
+    }
+
+    return request
+  })
+
   $axios.onError(error => {
     const code = parseInt(error.response && error.response.status)
     if (code === 401) {
